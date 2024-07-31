4367abfc5170ddb7e00ae7d05b90ffd20ef0115d
daigoro14 <daigoropersson@gmail.com>
2024-07-31 17:34:34 +0200

    home page layout decent

--- frontend/screens/HomeScreen.tsx
+++ frontend/screens/HomeScreen.tsx
@@ -1,12 +1,13 @@
 import Navigation from '@/components/Navigation'
 import React, { useState } from 'react'
-import { ImageBackground, StyleSheet, SafeAreaView, Text, View } from 'react-native'
+import { FlatList, StyleSheet,ScrollView, SafeAreaView, Text, View } from 'react-native'
 import FA from 'react-native-vector-icons/FontAwesome';
 import MCI from 'react-native-vector-icons/MaterialCommunityIcons';
 import FA6 from 'react-native-vector-icons/FontAwesome6';
+import AD from 'react-native-vector-icons/AntDesign';
+import II from 'react-native-vector-icons/Ionicons';
 import AnimatedComponent from '@/components/AnimatedComponent';
-import backgroundImage from '@/assets/images/IMG_9350.jpg';
-import cat from '@/assets/images/serious-cat-square-dog-photography.jpg';
+
 
 const activeColor = '#ff6f6f';
 
@@ -26,49 +27,58 @@ export default function HomeScreen() {
       icon: <FA6 name="people-group" style={styles.icon}/>
     },
     {
+      name: 'Food & Drinks',
+      icon: <II name="restaurant" style={styles.icon}/>
+    },
+    {
       name: 'History & Culture',
       icon: <MCI name="book-open-variant" style={styles.icon}/>
+    },
+    {
+      name: 'Health',
+      icon: <AD name="heart" style={styles.icon}/>
     }
   ]
 
   return (
     <SafeAreaView style={styles.container}>
-        <ImageBackground source={backgroundImage} style={styles.background}>
-          <View style={styles.content, styles.overlay}>
+        <ScrollView contentContainerStyle={styles.contentContainer}>
+          <View style={styles.content}>
+          {/* <FlatList
+          data={activities}
+          renderItem={({ item, index }) => <AnimatedComponent about={item} index={index} />}
+          keyExtractor={(item, index) => index.toString()}
+                /> */}
           {activities.map((activity, index) => {
             const borderColor = [activeColor, '#ffd70e', '#3a3a3a'][index % 3];
+            const additionalStyle = index !== 0 ? { marginTop: -60 } : {};
             return (
-              <View key={index} style={[styles.iconContainer, index % 2 === 0 ? styles.icon1 : styles.icon2]}>
+              <View key={index} style={[styles.iconContainer, index % 2 === 0 ? styles.icon1 : styles.icon2, additionalStyle]}>
                 <View style={[styles.circle, {borderColor}]} >{activity.icon}</View>
                 <Text>{activity.name}</Text>
               </View>
           )})}
           </View>
-        </ImageBackground>
+        </ScrollView>
         <Navigation currentPage="home"/>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  overlay: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Optional: overlay to improve text visibility
-  },
     container: {
         flex: 1,
         backgroundColor: '#fff'
     },
+      contentContainer: {
+      flexGrow: 1,
+      paddingHorizontal: 50,
+    },
     content: {
-        paddingHorizontal: 80,
-        flex: 1,
+        // paddingHorizontal: 80,
+      flex: 1,
+      height: 'auto',
+      justifyContent: 'center',
 
     },
     icon: {
